50c3140699a6b6eee4975ef202aee2bb35c0b7aa
Margaret Schaub <margaretcschaub@outlook.com>
2022-10-26 10:06:22 -1000

    added date conversion

---
+++ define_objects.py
@@ -0,0 +1,15 @@
+class Authors:
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+
+class Books:
+    def __init__(self, id_number, title, isbn, page_count, publication_date):
+        self.id_number = id_number
+        self.title = title
+        self.isbn = isbn
+        self.page_count = page_count
+        self.publication_date = publication_date
+
+    
