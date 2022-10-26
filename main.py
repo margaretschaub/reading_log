cf342b52ab236a506a1a693d48dd2e61fba1bac1
Margaret Schaub <margaretcschaub@outlook.com>
2022-10-25 21:22:20 -1000

    second commit

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
-file1 = open("/Users/margaretschaub/Desktop/books_table.txt","r")
+#"/Users/margaretschaub/Desktop/books_table.txt"
 
-books = file1.readlines()
+def parse_file(file_location):
 
-book_list = []
-for each in books:
-    new = each.replace(" ","")
-    new_2 = new.split(",")
-    book_list.append(new_2)
+    book_list = []
+    with open(file_location, "r") as f:
+       lines = f.readlines()
+       for line in lines:
+            raw_column_values = line.split(",")
+            # now we have the columns and can clean them up
+            row_list = []
+            for c in raw_column_values:
+                row_list.append(c.strip())
+
+            book_list.append(row_list)
+    return book_list
 
 
-# n=0
-
-# for each in book_list:
-#     try:
-#         book_list[n][0] = int(book_list[n][0])
-#         book_list[n][2] = int(book_list[n][2])
-#         book_list[n][3] = int(book_list[n][3])
-#     except(ValueError):
-#         print(f"Entry error at {book_list[n][1]}")
-#     n += 1
-
-def convert_integers(name_of_list,index_value):
-    n=0
+def convert_integers(name_of_list, index_value):
+    n = 0
     for each in name_of_list:
         try:
             name_of_list[n][index_value] = int(name_of_list[n][index_value])
-        except(ValueError):
+        except ValueError:
             print(f"Entry error at {name_of_list[n][1]}")
-    n += 1
 
-convert_integers(book_list,0)
-convert_integers(book_list,2)
-convert_integers(book_list,3)
+        n += 1
 
 
-# def import_and_make_lists(file_entry):
-#     book_list = []
-#     books = file_entry.readlines()
-#     for each in books:
-#         new = each.replace(" ", "")
-#         new_2 = new.split(",")
-#         book_list.append(new_2)
-#     return book_list
-#
-# print(import_and_make_lists(file1))
+cat = parse_file("/Users/margaretschaub/Desktop/books_table.txt")
+
+print(cat)
+print(type(cat))
+
+convert_integers(cat, 0)
+convert_integers(cat, 2)
+convert_integers(cat, 3)
